88d62fa3f1c5a3f2f0ea11ce276665285e3b8da9
Roman <115077525+RomanSzewczykC@users.noreply.github.com>
2025-04-04 10:43:50 +0200

    Update safe_trig_powers_test_suite_transition.py

--- safe_trig_powers_test_suite_transition.py
+++ safe_trig_powers_test_suite_transition.py
@@ -5,7 +5,29 @@ from safe_trig_powers import (
     safe_sin, safe_cos, safe_sin2, safe_cos2,
     safe_sin4, safe_cos4, safe_sin6, safe_cos6, safe_sin8, safe_cos8
 )
+"""
+MIT License
 
+Copyright (c) 2025 Roman Szewczyk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 def test_transition_smoothness():
     """Test the smoothness of transitions between polynomial approximation and NumPy implementation."""
     print("\n" + "=" * 80)
